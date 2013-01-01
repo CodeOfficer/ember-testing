c4d0df43e6722571fa202328235dcc45d28c2024
CodeOfficer <codeofficer@gmail.com>
2013-01-01 01:44:12 -0500

    spike

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,66 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: '<json:package.json>',
 
+    jshint: {
+      options: {
+        curly:   true,
+        eqeqeq:  true,
+        immed:   true,
+        latedef: true,
+        newcap:  true,
+        noarg:   true,
+        sub:     true,
+        undef:   true,
+        boss:    true,
+        eqnull:  true,
+        browser: true,
+        devel:   true,
+        debug:   true,
+        globals: {
+          jQuery:  true,
+          Ember:   true,
+          Em:      true,
+          DS:      true,
+          require: true,
+          App:     true,
+          params:  true
+        }
+      },
+
+      app:  ['Gruntfile.js', 'lib/app/**/*.js'],
+      test: ['test/spec_helper.js', 'test/support/**/*.js', 'test/specs/**/*.js']
+    },
+
     neuter: {
       app: {
         files: {'build/app.js': ['lib/app/**/*.js']}
       },
       test: {
         files: {'build/tests.js': ['test/spec_helper.js', 'test/support/**/*.js', 'test/specs/**/*.js']}
+      }
+    },
+
+    copy: {
+      json: {
+        options: {cwd: 'lib/app/json'},
+        files: {
+          'build/things.json': 'things.json',
+          'build/things/1.json': 'things/1.json',
+          'build/things/2.json': 'things/2.json',
+          'build/things/3.json': 'things/3.json'
+        }
+      },
+      app: {
+        options: {cwd: 'lib'},
+        files: {'build/': 'index.html'}
+      },
+      css: {
+        options: {cwd: 'lib/css'},
+        files: {'build/': 'app.css'}
+      },
+      test: {
+        options: {cwd: 'test'},
+        files: {'build/': 'test.html'}
       }
     },
 
@@ -28,53 +82,6 @@ module.exports = function(grunt) {
       }
     },
 
-    watch: {
-      files: ['Gruntfile.js', 'lib/**/*', 'test/**/*'],
-      tasks: ['app', 'test']
-    },
-
-    copy: {
-      app: {
-        options: {cwd: 'lib'},
-        files: {'build/': 'index.html'}
-      },
-      css: {
-        options: {cwd: 'lib/css'},
-        files: {'build/': 'app.css'}
-      },
-      test: {
-        options: {cwd: 'test'},
-        files: {'build/': 'test.html'}
-      }
-    },
-
-    jshint: {
-      options: {
-        curly:   true,
-        eqeqeq:  true,
-        immed:   true,
-        latedef: true,
-        newcap:  true,
-        noarg:   true,
-        sub:     true,
-        undef:   true,
-        boss:    true,
-        eqnull:  true,
-        browser: true,
-        devel:   true,
-        globals: {
-          jQuery:  true,
-          Ember:   true,
-          Em:      true,
-          require: true,
-          App:     true
-        }
-      },
-
-      app:  ['Gruntfile.js', 'lib/app/**/*.js'],
-      test: ['test/spec_helper.js', 'test/support/**/*.js', 'test/specs/**/*.js']
-    },
-
     ember_templates: {
       app: {
         options: {
@@ -84,6 +91,11 @@ module.exports = function(grunt) {
         },
         files: {'build/templates.js': ['lib/app/templates/**/*.hbs']}
       }
+    },
+
+    watch: {
+      files: ['Gruntfile.js', 'lib/**/*', 'test/**/*'],
+      tasks: ['app', 'test']
     }
 
   });
@@ -95,7 +107,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-neuter');
   grunt.loadNpmTasks('grunt-ember-templates');
 
-  grunt.registerTask('app', ['jshint:app', 'neuter:app', 'ember_templates:app', 'copy:app', 'copy:css']);
+  grunt.registerTask('app', ['jshint:app', 'neuter:app', 'ember_templates:app', 'copy:app', 'copy:css', 'copy:json']);
   grunt.registerTask('test', ['jshint:test', 'neuter:test', 'copy:test']);
   grunt.registerTask('vendor', ['concat:vendor', 'concat:test_vendor']);
 
