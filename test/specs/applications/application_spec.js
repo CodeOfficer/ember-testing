ca703d0e3a25b9a8703aa3a8ae30e3c43beca1ab
CodeOfficer <codeofficer@gmail.com>
2013-01-03 18:38:05 -0500

    tweak

--- test/specs/applications/application_spec.js
+++ test/specs/applications/application_spec.js
@@ -3,16 +3,16 @@ require('test/spec_helper');
 Ember.Application.prototype.reopen({
   reset: function() {
     console.log('-- reset');
-    App.set('_readinessDeferrals', 1);
-    App.set('isInitialized', false);
-    App.get('container').destroy();
-    App.buildContainer();
-    if (App.store) {
-      App.store.destroy();
-      App.set('store', null);
+    Todos.set('_readinessDeferrals', 1);
+    Todos.set('isInitialized', false);
+    Todos.get('container').destroy();
+    Todos.buildContainer();
+    if (Todos.store) {
+      Todos.store.destroy();
+      Todos.set('store', null);
     }
-    App.store = App.Store.create();
-    App.initialize();
+    Todos.store = Todos.Store.create();
+    Todos.initialize();
   }
 });
 
@@ -22,11 +22,11 @@ describe("Application", function() {
     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~');
     Ember.$("body").append("<div id='app'></div>");
     Ember.run(function() {
-      App.set('rootElement', '#app');
-      App.reset();
+      Todos.set('rootElement', '#app');
+      Todos.reset();
     });
-    console.log(App.get('container').lookup('router:main'));
-    console.log(App.get('eventDispatcher'));
+    console.log(Todos.get('container').lookup('router:main'));
+    console.log(Todos.get('eventDispatcher'));
     done();
   });
 
